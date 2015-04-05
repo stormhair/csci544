f32790853f41f0fe8855d5fb782c08a32bdccd18
Stephen Wong <bahamutstorm@gmail.com>
2015-04-04 21:12:52 -0700

    added project

---
+++ project/README.md
@@ -0,0 +1,1 @@
+#Project: Summarize reviews of professors on ratemyprofessor.com
---
+++ project/src/crawler.py
@@ -0,0 +1,10 @@
+'''
+	Crawl the required data
+	by Wenqiang Wang
+'''
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
