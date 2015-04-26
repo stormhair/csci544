9512603e2b7e41802111adbbca28484bb9c18319
Stephen Wong <bahamutstorm@gmail.com>
2015-04-26 15:37:47 -0700

    ignore .bat

---
+++ project/src/calc_results.bat
@@ -0,0 +1,45 @@
+echo 1128488
+python eval.py -t ..\data\baseline\1128488.txt -b ..\data\standard\1128488.standard.txt
+python eval.py -t ..\data\pred\1128488.txt -b ..\data\standard\1128488.standard.txt
+echo 1448190
+python eval.py -t ..\data\baseline\1448190.txt -b ..\data\standard\1448190.standard.txt
+python eval.py -t ..\data\pred\1448190.txt -b ..\data\standard\1448190.standard.txt
+echo 683089
+python eval.py -t ..\data\baseline\683089.txt -b ..\data\standard\683089.standard.txt
+python eval.py -t ..\data\pred\683089.txt -b ..\data\standard\683089.standard.txt
+echo 982800
+python eval.py -t ..\data\baseline\982800.txt -b ..\data\standard\982800.standard.txt
+python eval.py -t ..\data\pred\982800.txt -b ..\data\standard\982800.standard.txt
+echo 1904673
+python eval.py -t ..\data\baseline\1904673.txt -b ..\data\standard\1904673.standard.txt
+python eval.py -t ..\data\pred\1904673.txt -b ..\data\standard\1904673.standard.txt
+echo 1406735
+python eval.py -t ..\data\baseline\1406735.txt -b ..\data\standard\1406735.standard.txt
+python eval.py -t ..\data\pred\1406735.txt -b ..\data\standard\1406735.standard.txt
+echo 1335974
+python eval.py -t ..\data\baseline\1335974.txt -b ..\data\standard\1335974.standard.txt
+python eval.py -t ..\data\pred\1335974.txt -b ..\data\standard\1335974.standard.txt
+echo 358799
+python eval.py -t ..\data\baseline\358799.txt -b ..\data\standard\358799.standard.txt
+python eval.py -t ..\data\pred\358799.txt -b ..\data\standard\358799.standard.txt
+echo 816694
+python eval.py -t ..\data\baseline\816694.txt -b ..\data\standard\816694.standard.txt
+python eval.py -t ..\data\pred\816694.txt -b ..\data\standard\816694.standard.txt
+echo 1813521
+python eval.py -t ..\data\baseline\1813521.txt -b ..\data\standard\1813521.standard.txt
+python eval.py -t ..\data\pred\1813521.txt -b ..\data\standard\1813521.standard.txt
+echo 1463640
+python eval.py -t ..\data\baseline\1463640.txt -b ..\data\standard\1463640.standard.txt
+python eval.py -t ..\data\pred\1463640.txt -b ..\data\standard\1463640.standard.txt
+echo 1433608
+python eval.py -t ..\data\baseline\1433608.txt -b ..\data\standard\1433608.standard.txt
+python eval.py -t ..\data\pred\1433608.txt -b ..\data\standard\1433608.standard.txt
+echo 1342582
+python eval.py -t ..\data\baseline\1342582.txt -b ..\data\standard\1342582.standard.txt
+python eval.py -t ..\data\pred\1342582.txt -b ..\data\standard\1342582.standard.txt
+echo 1514243
+python eval.py -t ..\data\baseline\1514243.txt -b ..\data\standard\1514243.standard.txt
+python eval.py -t ..\data\pred\1514243.txt -b ..\data\standard\1514243.standard.txt
+echo 200155
+python eval.py -t ..\data\baseline\200155.txt -b ..\data\standard\200155.standard.txt
+python eval.py -t ..\data\pred\200155.txt -b ..\data\standard\200155.standard.txt
